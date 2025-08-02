6048de2d26093ae0f0bd6a71d3fd0dca6fe45dc3
channangan <channangan148@gmail.com>
2025-08-02 23:30:09 +0700

    first commit

---
+++ wix-app/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-screen h-screen grid place-content-center bg-teal-600">
+      App
+    </div>
+  );
+};
+
+export default App;
---
+++ wix-app/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <div className="logo"></div>
+        <ul>
+          <li>Prouct</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
